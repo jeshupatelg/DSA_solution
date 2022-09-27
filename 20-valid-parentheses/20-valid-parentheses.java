7c3603a23a5752e816e24dbebac3ee4c58a9ff8a
Jeshu Patel <103898120+jeshupatelg@users.noreply.github.com>
2022-09-28 00:12:28 +0530

    Time: 4 ms (40.17%), Space: 41.8 MB (73.38%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isValid(String s) {
+        if (s.length()%2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){stack.push(s.charAt(i));}
@@ -19,7 +20,7 @@ class Solution {
                 }
             }
         }
-        if(!stack.isEmpty()){return false;}
-        return true;
+        //if(!stack.isEmpty()){return false;}
+        return stack.isEmpty();
     }
 }
